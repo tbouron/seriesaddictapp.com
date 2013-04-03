53e610838ebc783ecf45718b22c2b5d296eb96d2
Tomtom33 <tbouron@gmail.com>
2013-04-03 17:17:25 +0200

    Changelog finally comes automatically from bitbucket too \o/

---
+++ js/changelog.js
@@ -0,0 +1,51 @@
+$(function() {
+	$.ajax({
+		url: "https://bitbucket.org/eltibouron/seriesaddictstatic/raw/afca5f7ebeb53e9a90b8a8193d529424ff8747af/CHANGELOG",
+		type: "get",
+		success: function(res) {
+			var html = [];
+			$.each($(res.responseText).children(), function(i, versionNode) {
+				var version = $(versionNode);
+				html.push("<h3>Version " + version.attr('code') + " (" + version.attr('date') + ")</h3>");
+				
+				if (version.children().size() > 0) {
+					html.push("<ul>");
+					$.each(version.children(), function (j, changeNode) {
+						var text = null;
+						var className = null;
+						var prefix = null;
+						var isPremium = false;
+						var change = $(changeNode);
+						
+						if (change.attr('premium') == 'true') {
+							isPremium = true;
+						}
+						
+						if (change.is('add')) {
+							className = " label-info";
+							prefix = "Ajout";
+						} else if (change.is('update')) {
+							className = "";
+							prefix = "Mise à jour";
+						} else if (change.is('bugfix')) {
+							className = ' label-important';
+							prefix = 'Correction de bug';
+						}
+						
+						html.push(
+							'<li>' +
+							(className != null 
+								? '<span class="label' + className + '">' + prefix + '</span> ' + (isPremium ? '<span class="label label-inverse">Fonctionnalité premium</span> ' : '')
+								: ""
+							) +
+							change.html() + 
+							'</li>'
+						);
+					});
+					html.push("</ul>");
+				}
+			});
+			$('#changelog .container').html(html.join("\n")).removeClass('loading-changelog');
+		}
+	});
+});
